a8d9b1d4cbd4c49e44902ec520bce1698261da85
buenalves <alvesbgiovana@gmail.com>
2022-11-23 15:17:01 -0300

    organizacao das pastas

---
+++ view/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+import uuid
+from werkzeug.utils import secure_filename
+import urllib.request
+
+#inicializa o flask
+app = Flask(__name__)
+
+#diretorio onde serão salvas as imagens
+UPLOAD_FOLDER = '\salvaImagem'
+
+app.secret_key = "projetonextt3"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+ALLOWED_EXTENSIONS = {'jpg'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    
+@app.route("/")
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route("/", methods = ['POST'])
+def upload_image():
+    
+    if 'files[]' not in request.files:
+        flash('Nenhum arquivo encontrado')
+        
+    files = request.files.getlist('files[]')
+    file_names =[]
+    
+    for file in files:
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_names.append(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+        else:
+            flash('Só são permitidas imagens com extensão .jpg.')
+            return redirect(request.url)
+        
+    return render_template('upload.html', filenames = file_names)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename = 'uploads/' + filename), code = 301)
+
+if __name__ == "__main__":
+    app.run(debug=True)
