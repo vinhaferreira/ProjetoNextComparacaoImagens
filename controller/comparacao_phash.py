a7285ce54b33dac906cc07027c26c56e90409bda
gustaavogalvao <gustaavogalvao0@gmail.com>
2022-11-25 15:09:50 -0300

    alteração do average_hash para phash

--- controller/comparacao_phash.py
+++ controller/comparacao_phash.py
@@ -2,11 +2,11 @@ from PIL import Image
 #Importando dependência
 import imagehash
 #Criando número Hash da primeira imagem
-hash = imagehash.average_hash(Image.open(r'C:\Users\Gustavo\Desktop\projetoNEXT\Phash\images/1.png'),hash_size= 10)
+hash = imagehash.phash(Image.open(r'C:\Users\Gustavo\Desktop\projetoNEXT\Phash\images/1.png'),hash_size= 10)
 print ('Imagem 1: ' + str(hash))
 
 #Criando número Hash da segunda imagem
-otherhash = imagehash.average_hash(Image.open(r'C:\Users\Gustavo\Desktop\projetoNEXT\Phash\images/2.png'),hash_size= 10)
+otherhash = imagehash.phash(Image.open(r'C:\Users\Gustavo\Desktop\projetoNEXT\Phash\images/2.png'),hash_size= 10)
 print('Imagem 2:' + str(otherhash))
 
 #Transformando a diferença entre as imgaens em string (quando estava em int, estava apresentando erro)
